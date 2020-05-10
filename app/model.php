075fae75bcc53937167420f20cf8c06ae61cb8b4
AFuwape Idris <afuidris@gmail.com>
2020-05-10 19:35:28 +0100

    Added the post handler to the root controller

--- app/model.php
+++ app/model.php
@@ -1,43 +1,72 @@
-<?php 
+<?php
 
 class Model
 {
-	public $db;
+    public $db;
 
-	public function __construct()
-	{
-		$config = include "config.php";
-		$config = (object) $config['database'];
-		$this->db = new mysqli($config->host, $config->user, $config->pass, $config->db);
-	}
+    public function __construct()
+    {
+        $config = include "config.php";
+        $config = (object) $config['database'];
 
-	public function select_many($table, $where = '', $orderBy = '', )
-	{
-		$query = "SELECT * FROM ".$table." ";
+        $this->db = new mysqli($config->host, $config->user, $config->pass, $config->db);
+    }
 
-		if($where != "") {
-			$query .= $where;
-		}
+    public function select_many($table, $where = '', $orderBy = '' )
+    {
+        $query = "SELECT * FROM ".$table." ";
 
-		$query = $this->con($query);
+        if($where != "") {
+            $query .= " WHERE ".$where;
+        }
 
-		$data = [];
+        if($orderBy != "") {
+            $query .= " ORDER BY ".$orderBy;
+        }
 
-		if($query->num_rows > 0) {
-			while ($row = $query->fetch_object()) {
-				$data[] = $row;
-			}
-		}
+        $query = $this->db->query($query);
 
-		return $data;
-	}
+        $data = [];
 
-	public function select_one($table, $where) {
-		$query = "SELECT * FROM ".$table. " ". $where;
+        if($query->num_rows > 0) {
+            while ($row = $query->fetch_object()) {
+                $data[] = $row;
+            }
+        }
 
-		$query = $this->con->query($query);
+        return $data;
+    }
 
-		return $query->fetch_object();
+    public function select_one($table, $where) {
+        $query = "SELECT * FROM ".$table. " WHERE ". $where;
 
-	}
+        $query = $this->db->query($query);
+
+        if($query->num_rows > 0)
+            return $query->fetch_object();
+
+        return [];
+
+    }
+
+    public function insert($table, $data)
+    {
+        $query = "INSERT INTO ".$table."(";
+
+        foreach($data as $key => $val) {
+            $query .= $key.",";
+        }
+
+        $query = substr($query, 0, -1).") VALUES(";
+
+        foreach($data as $value) {
+            $query .= "'".$this->db->escape_string($value)."',";
+        }
+
+        $query = substr($query, 0, -1).")";
+
+        $this->db->query($query);
+
+        return true;
+    }
 }
